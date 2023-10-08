ceda983579ff813978d5b6267a2f2cfe713957ef
v-sivozhelezov <sivozhelezov.v@gmail.com>
2023-10-08 23:05:07 +0300

    Добавлен компонент NavMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,15 @@
 import './App.css'
 import AudioPlayer from './components/AudioPlayer'
 import './components/AudioPlayer.css'
+import NavMenu from './components/NavMenu'
+import './components/NavMenu.css'
 
 function App() {
   return (
     <div className="wrapper">
       <div className="container">
         <main className="main">
-          <nav className="main__nav nav">
-            <div className="nav__logo logo">
-              <img className="logo__image" src="/img/logo.png" alt="logo" />
-            </div>
-            <div className="nav__burger burger">
-              <span className="burger__line" />
-              <span className="burger__line" />
-              <span className="burger__line" />
-            </div>
-            <div className="nav__menu menu">
-              <ul className="menu__list">
-                <li className="menu__item">
-                  <a href="/#" className="menu__link">
-                    Главное
-                  </a>
-                </li>
-                <li className="menu__item">
-                  <a href="/#" className="menu__link">
-                    Мой плейлист
-                  </a>
-                </li>
-                <li className="menu__item">
-                  <a href="../signin.html" className="menu__link">
-                    Войти
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </nav>
+          {NavMenu()}
           <div className="main__centerblock centerblock">
             <div className="centerblock__search search">
               <svg className="search__svg">
