7e826c30df85d09d2478aebed17499cd3d6b881b
v-sivozhelezov <sivozhelezov.v@gmail.com>
2023-11-15 00:13:17 +0300

    Выполнены  роуты и редиректы

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import GlobalStyle from './GlobalStyle.styles'
-// import MainPage from './pages/MainPage'
-// import tracks from './components/data/tracks'
 import AppRoutes from './routes'
 
 function App() {
