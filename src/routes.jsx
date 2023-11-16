36d58f2978a10f53669a79e53749a46531fa3fd0
v-sivozhelezov <sivozhelezov.v@gmail.com>
2023-11-16 22:49:46 +0300

    Исправлены замечания

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,16 +6,8 @@ import FavoritesPage from './pages/FavoritesPage/FavoritesPage'
 import RegisterPage from './pages/RegisterPage/RegisterPage'
 import CategoryPage from './pages/CategoryPage/CategoryPage'
 import ProtectedRoute from './components/protected-route/protected-route'
-import MainPage from './pages/MainPage/MainPage'
-import LoginPage from './pages/LoginPage/LoginPage'
-import NotFound from './pages/NotFound/NotFound'
-import FavoritesPage from './pages/FavoritesPage/FavoritesPage'
-import RegisterPage from './pages/RegisterPage/RegisterPage'
-import CategoryPage from './pages/CategoryPage/CategoryPage'
-import ProtectedRoute from './components/protected-route/protected-route'
 
 export default function AppRoutes({ handleLoginButtonClick }) {
-
   return (
     <Routes>
       <Route path="/register" element={<RegisterPage />} />
